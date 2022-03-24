3877fc3ee7cb384fd0904e37019f0d65f268cbb8
melek <malekgomri881@hotmail.com>
2022-03-25 00:46:35 +0100

    l48

--- pfe8-master/src/Controller/ProductController.php
+++ pfe8-master/src/Controller/ProductController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Classe\Search;
+use App\Entity\Comments;
+use App\Form\CommentsType;
 use App\Entity\Product;
 use App\Form\SearchType;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -34,13 +37,45 @@ class ProductController extends AbstractController
           ]);
       }
       #[Route('produit/{slug}', name: 'product')]
-      public function show($slug){
+      public function show($slug,EntityManagerInterface $em, Request $request){
         $product=$this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
         if (! $product){
         return $this->redirectToRoute('products');
         }
+         // Partie commentaires
+        // On crée le commentaire "vierge"
+        $comment = new Comments;
+
+        // On génère le formulaire
+        $commentForm = $this->createForm(CommentsType::class, $comment);
+
+        $commentForm->handleRequest($request);
+
+        // Traitement du formulaire
+        if($commentForm->isSubmitted() && $commentForm->isValid()){
+            $comment->setCreatedAt(new DateTime());
+            $comment->setProduct($product);
+
+            // On récupère le contenu du champ parentid
+            $parentid = $commentForm->get("parentid")->getData();
+
+            // On va chercher le commentaire correspondant
+
+            if($parentid != null){
+                $parent = $em->getRepository(Comments::class)->find($parentid);
+            }
+
+            // On définit le parent
+            $comment->setParent($parent ?? null);
+
+            $em->persist($comment);
+            $em->flush();
+            $this->addFlash('message', 'Votre commentaire a bien été envoyé');
+        }
+
         return $this->render('product/show.html.twig',[
-            'product' => $product
+            'product' => $product,
+            'commentForm' => $commentForm->createView()
         ]);
       }
 
--- pfe8-master/src/Entity/Product.php
+++ pfe8-master/src/Entity/Product.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ProductRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
@@ -34,6 +36,14 @@ class Product
     #[ORM\ManyToOne(targetEntity: Category::class, inversedBy: 'products')]
     #[ORM\JoinColumn(nullable: false)]
     private $category;
+
+    #[ORM\OneToMany(mappedBy: 'product', targetEntity: Comments::class, orphanRemoval: true)]
+    private $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -120,6 +130,36 @@ class Product
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comments>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comments $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comments $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getProduct() === $this) {
+                $comment->setProduct(null);
+            }
+        }
 
         return $this;
     }
